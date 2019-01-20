b6695866d88c1b4bc2b512af798c9f44cce0f3c7
Oleg Strunevskiy <oleg.strunevskiy@gmail.com>
2019-01-20 17:45:05 -0600

    Add debug information to deployment script

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-if [[ $TRAVIS_EVENT_TYPE == “push” ]] && [[ $TRAVIS_BRANCH == “master” ]] && [[ $TRAVIS_PULL_REQUEST == “false” ]]
+if [[ $TRAVIS_EVENT_TYPE == "push" ]] && [[ $TRAVIS_BRANCH == "master" ]] && [[ $TRAVIS_PULL_REQUEST == "false" ]]
 then
-    echo ""
+    echo "${TRAVIS_EVENT_TYPE}"
+    echo "${TRAVIS_BRANCH}"
+    echo "${TRAVIS_PULL_REQUEST}"
     mvn clean deploy --settings .travis/settings.xml -B -U -P release
 else
-    echo ""
+    echo "${TRAVIS_EVENT_TYPE}"
+    echo "${TRAVIS_BRANCH}"
+    echo "${TRAVIS_PULL_REQUEST}"
     mvn clean package -B -U
 fi
