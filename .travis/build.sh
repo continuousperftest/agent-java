250d629096c4ebaa8d6a0070a2d3a0d8106eddd8
Oleg Strunevskiy <oleg.strunevskiy@gmail.com>
2019-01-20 17:28:27 -0600

    Update deploy script

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_PULL_REQUEST == “false” ]] && [[ $TRAVIS_BRANCH == “master” ]]
+if [[ $TRAVIS_EVENT_TYPE == “push” ]] && [[ $TRAVIS_BRANCH == “master” ]] && [[ $TRAVIS_PULL_REQUEST == “false” ]]
 then
     echo ""
     mvn clean deploy --settings .travis/settings.xml -B -U -P release
